2ad76405e3a0bc5927c70bcd069ba780df10a34d
Bhavesh Ramburn <nitrogen@gmail.com>
2016-12-19 02:24:36 +0000

    Update META output

--- icelabz_yoast_seo_hook.php
+++ icelabz_yoast_seo_hook.php
@@ -23,10 +23,6 @@ function icelabz_yoast_seo_hook()
 {
 
     $options = get_option('wpseo_video');
-    if ($options['facebook_embed'] !== true) {
-
-        return false;
-    }
 
     if (is_singular()) {
         // get post global info
@@ -51,12 +47,57 @@ function icelabz_yoast_seo_hook()
     // let's check if this is Vimeo video.
     $domain_to_check = parse_url($video['url'], PHP_URL_HOST);
 
+    $vimeo_data = Get_Vimeo_data($video['url']);
+
     if ($domain_to_check == 'vimeo.com') {
+
+        // add thumbnail:
+        if ($options['facebook_embed'] !== true) {
+            echo '<meta property="og:image" content="' . esc_attr($vimeo_data['thumbnail_url']) . '" />' . "\n";
+            echo '<meta property="og:image:type" content="image/jpg" />' . "\n";
+            echo '<meta property="og:image:width" content="' . esc_attr($vimeo_data['thumbnail_width']) . '">' . "\n";
+            echo '<meta property="og:image:height" content="' . esc_attr($vimeo_data['thumbnail_height']) . '">' . "\n";
+
+        }
         // if the video is Vimeo then we process.
 
         echo '<meta property="og:video:url" content="' . esc_attr($video['player_loc']) . '?autoplay=1" />' . "\n";
         echo '<meta property="og:video:secure_url" content="' . esc_attr($video['player_loc']) . '?autoplay=1" />' . "\n";
+        echo '<meta property="og:video:type" content="text/html" />' . "\n";
+        echo '<meta property="og:video:width" content="' . esc_attr($vimeo_data['width']) . '">' . "\n";
+        echo '<meta property="og:video:height" content="' . esc_attr($vimeo_data['height']) . '">' . "\n";
+
+        $re = '/video\/(\d+)/s';
+        preg_match($re, $video['player_loc'], $matches);
+
+        // add more meta for vimeo
+
+        echo '<meta property="og:video:url" content="https://vimeo.com/moogaloop.swf?clip_id=' . $matches[1] . '&amp;autoplay=1">' . "\n";
+        echo '<meta property="og:video:secure_url" content="https://vimeo.com/moogaloop.swf?clip_id=' . $matches[1] . '&amp;autoplay=1">' . "\n";
+        echo '<meta property="og:video:type" content="application/x-shockwave-flash">' . "\n";
+        echo '<meta property="og:video:width" content="' . esc_attr($vimeo_data['width']) . '">' . "\n";
+        echo '<meta property="og:video:height" content="' . esc_attr($vimeo_data['height']) . '">' . "\n";
+
     }
+
+}
+
+/**
+ * Gets additional information from Vimeo
+ *
+ * @return array
+ * @author bramburn
+ **/
+function Get_Vimeo_data($url)
+{
+
+    $response = wp_remote_get('https://vimeo.com/api/oembed.json?url=' . esc_url_raw($url));
+
+/* Will result in $api_response being an array of data,
+parsed from the JSON response of the API listed above */
+    $api_response = json_decode(wp_remote_retrieve_body($response), true);
+    // print_r($api_response);
+    return $api_response;
 
 }
 
